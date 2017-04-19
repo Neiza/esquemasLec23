a7556ed42793b3feee16e90a1e0c138bbcae0f76
Neiza Nunez <neizapf@gmail.com>
2017-04-19 10:25:13 -0500

    js y css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,28 @@
-var nDivs = 2;
+var nDivs = 1;
 var nChildDivs = 1;
 var nChild2divs = 1
-var body = document.getElementById("body")
+var container = document.getElementById("container")
 
-function createDivs(){
+function createDivs(nDivs,nChildDivs,nChild2divs,padres,hijos,nietos){
   for(var i=0; i<nDivs; i++){
       var  div = document.createElement("div");
-      div.classList.add("padres");
+      div.classList.add(padres);
       body.appendChild(div);
       for(j=0; j<nChildDivs; j++){
         var childDiv = document.createElement("div");
-        childDiv.classList.add("hijos");
+        childDiv.classList.add(hijos);
         div.appendChild(childDiv);
           for (var k=0; k<nChild2divs; k++){
             Child2div = document.createElement("div");
-            Child2div.classList.add("nietos")
+            Child2div.classList.add(nietos)
             childDiv.appendChild(Child2div)
           }
       }
   }
+
 }
 
-createDivs();
+
+
+createDivs(1,1,1,"padre1", "hijo1", "nieto1");
+createDivs(1,1,1,"padre2", "hijo2", "nieto2");
